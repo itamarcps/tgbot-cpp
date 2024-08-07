455bd4288fe89a38df53754fc2986975e39fd4c5
Itamar Carvalho <32653934+itamarcps@users.noreply.github.com>
2024-08-06 21:42:38 -0300

    Replace boost asio with boost beast

--- src/net/BoostHttpOnlySslClient.cpp
+++ src/net/BoostHttpOnlySslClient.cpp
@@ -4,10 +4,17 @@
 
 #include <cstddef>
 #include <vector>
+#include <iostream>
+
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
 
 using namespace std;
-using namespace boost::asio;
-using namespace boost::asio::ip;
+using tcp = boost::asio::ip::tcp;
+namespace http = boost::beast::http;
 
 namespace TgBot {
 
@@ -17,71 +24,63 @@ BoostHttpOnlySslClient::BoostHttpOnlySslClient() : _httpParser() {
 BoostHttpOnlySslClient::~BoostHttpOnlySslClient() {
 }
 
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+
+using tcp = boost::asio::ip::tcp;
+namespace http = boost::beast::http;
+
 string BoostHttpOnlySslClient::makeRequest(const Url& url, const vector<HttpReqArg>& args) const {
-    tcp::resolver resolver(_ioService);
-    tcp::resolver::query query(url.host, "8081"); // Use port 8081 for HTTP, local server
+    try {
+        // The I/O context is required for all I/O
+        boost::asio::io_context ioc;
 
-    tcp::socket socket(_ioService);
+        // These objects perform our I/O
+        tcp::resolver resolver(ioc);
+        tcp::socket socket(ioc);
 
-    connect(socket, resolver.resolve(query));
+        // Look up the domain name
+        auto const results = resolver.resolve(url.host, "80");
 
-    #ifdef TGBOT_DISABLE_NAGLES_ALGORITHM
-    socket.set_option(tcp::no_delay(true));
-    #endif //TGBOT_DISABLE_NAGLES_ALGORITHM
-    #ifdef TGBOT_CHANGE_SOCKET_BUFFER_SIZE
-    #if _WIN64 || __amd64__ || __x86_64__ || __MINGW64__ || __aarch64__ || __powerpc64__
-    socket.set_option(socket_base::send_buffer_size(65536));
-    socket.set_option(socket_base::receive_buffer_size(65536));
-    #else //for 32-bit
-    socket.set_option(socket_base::send_buffer_size(32768));
-    socket.set_option(socket_base::receive_buffer_size(32768));
-    #endif //Processor architecture
-    #endif //TGBOT_CHANGE_SOCKET_BUFFER_SIZE
+        // Make the connection on the IP address we get from a lookup
+        boost::asio::connect(socket, results.begin(), results.end());
 
-    string requestText = _httpParser.generateRequest(url, args, false);
-    write(socket, buffer(requestText.c_str(), requestText.length()));
+        // Set up an HTTP GET request message
+        http::request<http::string_body> req{http::verb::get, "/", 11};
+        req.set(http::field::host, url.host);
+        req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
 
-    fd_set fileDescriptorSet;
-    struct timeval timeStruct;
-    
-    // set the timeout to 20 seconds
-    timeStruct.tv_sec = _timeout;
-    timeStruct.tv_usec = 0;
-    FD_ZERO(&fileDescriptorSet);
-    
-    int nativeSocket = static_cast<int>(socket.native_handle());
-    
-    FD_SET(nativeSocket, &fileDescriptorSet);        
-    select(nativeSocket + 1, &fileDescriptorSet, NULL, NULL, &timeStruct);
-    
-    if (!FD_ISSET(nativeSocket, &fileDescriptorSet)) { // timeout
-        std::string sMsg("TIMEOUT on read client data. Client IP: ");
-        sMsg.append(socket.remote_endpoint().address().to_string());
-        _ioService.reset();
-        
-        throw std::exception();
-    }      
-    
-    string response;
+        // Send the HTTP request to the remote host
+        http::write(socket, req);
 
-    #ifdef TGBOT_CHANGE_READ_BUFFER_SIZE
-    #if _WIN64 || __amd64__ || __x86_64__ || __MINGW64__ || __aarch64__ || __powerpc64__
-    char buff[65536];
-    #else //for 32-bit
-    char buff[32768];
-    #endif //Processor architecture
-    #else
-    char buff[1024];
-    #endif //TGBOT_CHANGE_READ_BUFFER_SIZE
+        // This buffer is used for reading and must be persisted
+        boost::beast::flat_buffer buffer;
 
-    boost::system::error_code error;
-    while (!error) {
-        std::size_t bytes = read(socket, buffer(buff), error);
-        response += string(buff, bytes);
+        // Declare a container to hold the response
+        http::response<http::dynamic_body> res;
+
+        // Receive the HTTP response
+        http::read(socket, buffer, res);
+
+        // Gracefully close the socket
+        boost::system::error_code ec;
+        socket.shutdown(tcp::socket::shutdown_both, ec);
+
+        // If we get here then the connection is closed gracefully
+
+        // Return the body of the response as a string
+        return boost::beast::buffers_to_string(res.body().data());
     }
-
-    return _httpParser.extractBody(response);
+    catch (std::exception const& e) {
+        // Handle exceptions here
+        std::cout << "Error: " << e.what() << std::endl;
+        throw;
+    }
 }
+
 
 
 }
